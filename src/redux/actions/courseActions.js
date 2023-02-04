9f7437168d9e6db8fbc6c63d1f58aa9f8bc5bc45
Ben Mishali <ben9992@gmail.com>
2023-02-04 12:49:39 +0200

    start redux async api middleware with first thunk

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,24 @@
 import * as types from "./actionTypes";
-
+import * as courseApi from "../../api/courseApi";
 export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
+	return { type: types.CREATE_COURSE, course };
+}
+
+// thunk success
+export function loadCoursesSuccess(courses) {
+	return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+// course api thunk
+export function loadCourses() {
+	return function (dispatch) {
+		return courseApi
+			.getCourses()
+			.then((courses) => {
+				dispatch(loadCoursesSuccess(courses));
+			})
+			.catch((err) => {
+				throw err;
+			});
+	};
 }
